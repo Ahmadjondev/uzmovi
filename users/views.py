de7500fef91b9166e9884034e9a83bbc2a2f4728
ahmadjondev <ahmadjondevv@gmail.com>
2025-04-12 20:13:56 +0500

    Refactor user profile view to remove unused profile form and user activity sections; update template to streamline user experience

--- users/views.py
+++ users/views.py
@@ -2,34 +2,21 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.contrib.auth.models import User
-from .forms import ProfileUpdateForm, UserUpdateForm
-from movies.models import Watchlist, Rating, Comment
+from .forms import UserUpdateForm
 
 @login_required
 def profile(request):
     if request.method == 'POST':
         u_form = UserUpdateForm(request.POST, instance=request.user)
-        p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
 
-        if u_form.is_valid() and p_form.is_valid():
+        if u_form.is_valid():
             u_form.save()
-            p_form.save()
             messages.success(request, 'Your profile has been updated!')
             return redirect('profile')
     else:
         u_form = UserUpdateForm(instance=request.user)
-        p_form = ProfileUpdateForm(instance=request.user.profile)
-
-    # Get user activity
-    watchlist = Watchlist.objects.filter(user=request.user).order_by('-added_at')[:5]
-    ratings = Rating.objects.filter(user=request.user).order_by('-created_at')[:5]
-    comments = Comment.objects.filter(user=request.user).order_by('-created_at')[:5]
 
     context = {
         'u_form': u_form,
-        'form': p_form,
-        'watchlist': watchlist,
-        'ratings': ratings,
-        'comments': comments
     }
     return render(request, 'users/profile.html', context)
